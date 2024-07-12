e3e4c0344a05395b8d96fd655e5b126be84dba60
gauravRandheer <gaurav.shrivastava.15csc@bml.edu.in>
2024-07-12 21:12:09 +0530

    Footer Readjust the height

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -55,7 +55,7 @@ function Home() {
       <div className="w-full py-8">
         <Container>
           <div className="flex flex-wrap">
-            {posts.map((post) => (
+            {posts?.map((post) => (
               <div key={post.$id} className="p-2 w-1/4">
                 <PostCard {...post} />
               </div>
@@ -84,7 +84,7 @@ function Home() {
 
   if (loginstatus == false) {
     return (
-      <div className="w-full py-8 mt-auto text-center">
+      <div className="w-full py-8 mt-auto text-center h-[42vh]">
         <Container>
           <div className="flex">
             <div className="p-2 w-full">
